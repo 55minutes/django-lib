4c19efb1de754b1c52a66400c7ea0c66054fa62f
gsong <george@55minutes.com>
2009-11-26 07:33:43 +0000

    Adding setup for this package.
    git-svn-id: file:///opt/custom/var/svn/repos/internal/fiftyfive/python/trunk/django/lib@8788 07aa3797-eaea-0310-9497-ec20f46c85ba

---
+++ setup.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import codecs
+import os
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+
+DEPENDENCIES = ['django>=1.0']
+HOME_URL = 'https://internal.55minutes.com/svn/fiftyfive/python/trunk/django/lib/'
+
+short_description = '55 Minutes libraries for use with the Django framework.'
+if os.path.exists("README.rst"):
+    long_description = codecs.open("README.rst", "r", "utf-8").read()
+else:
+    long_description = short_description
+
+
+# Setup the project directory
+setup(
+    name='55M Django',
+    version='0.5',
+    author='55 Minutes',
+    author_email='info@55minutes.com',
+    maintainer='55 Minutes',
+    maintainer_email='info@55minutes.com',
+    url=HOME_URL,
+    description=short_description,
+    long_description=long_description,
+    download_url=HOME_URL,
+    platforms=["any"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: Other/Proprietary License",
+        "Operating System :: OS Independent",
+        "Operating System :: POSIX",
+        "Programming Language :: Python",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilties",
+    ],
+    license="All Rights Reserved (c) 2010 55 Minutes",
+    package_dir={'':'src'},
+
+    packages=find_packages('src', exclude=['ez_setup']),
+    namespace_packages=['fiftyfive'],
+    zip_safe=False,
+    install_requires=DEPENDENCIES,
+)
