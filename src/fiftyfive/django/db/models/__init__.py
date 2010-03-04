cb562ffca83336c64a146ba21891bddc8b78086b
gsong <george@55minutes.com>
2010-03-04 23:03:10 +0000

    Added a new permalink decorator which accepts all the kwargs that reverse() accepts.
    git-svn-id: file:///opt/custom/var/svn/repos/internal/fiftyfive/python/trunk/django/lib@8971 07aa3797-eaea-0310-9497-ec20f46c85ba

--- src/fiftyfive/django/db/models/__init__.py
+++ src/fiftyfive/django/db/models/__init__.py
@@ -1,1 +1,12 @@
 from current_user import *
+
+def permalink_with_prefix(func):
+    """
+    This works exactly like Django's permalink decorator, except it takes an
+    extra prefix kwarg which is passwed along to reverse().
+    """
+    from django.core.urlresolvers import reverse
+    def inner(*args, **kwargs):
+        bits = func(*args, **kwargs)
+        return reverse(bits[0], None, *bits[1:])
+    return inner
