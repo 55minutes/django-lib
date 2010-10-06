b76b0debc3e759e974c186d558350a5c99c1fb15
gsong <george@55minutes.com>
2010-10-06 18:42:37 +0000

    Updated `permalink_with_prefix` to work properly.
    git-svn-id: file:///opt/custom/var/svn/repos/internal/fiftyfive/python/trunk/django/lib@9213 07aa3797-eaea-0310-9497-ec20f46c85ba

--- src/fiftyfive/django/db/models/__init__.py
+++ src/fiftyfive/django/db/models/__init__.py
@@ -3,10 +3,17 @@ from current_user import *
 def permalink_with_prefix(func):
     """
     This works exactly like Django's permalink decorator, except it takes an
-    extra prefix kwarg which is passwed along to reverse().
+    extra prefix kwarg which is passwed along to reverse(). We also strip out
+    the script prefix and make sure the supplied prefix ends with '/'.
     """
-    from django.core.urlresolvers import reverse
+    from urlparse import urljoin
+    from django.core.urlresolvers import reverse, get_script_prefix
     def inner(*args, **kwargs):
         bits = func(*args, **kwargs)
-        return reverse(bits[0], None, *bits[1:])
+        script_prefix = get_script_prefix()
+        prefix = bits[0]
+        if not prefix.endswith('/'): prefix += u'/'
+        bits = bits[1:]
+        link = reverse(bits[0], None, *bits[1:3]).replace(script_prefix, u'', 1)
+        return urljoin(prefix, link)
     return inner
